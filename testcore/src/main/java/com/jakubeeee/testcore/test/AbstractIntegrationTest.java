3a159c00617be58a3d8680f36380837e33ce9c53
Jakub Bereźnicki <jakub.bereznicki@gmail.com>
2019-08-17 21:46:38 +0200

    Added abstract superclass and properties for integration tests

---
+++ testcore/src/main/java/com/jakubeeee/testcore/test/AbstractIntegrationTest.java
@@ -0,0 +1,46 @@
+package com.jakubeeee.testcore.test;
+
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.jakubeeee.Application;
+import com.jakubeeee.testcore.container.CustomPostgreSQL12Container;
+import com.jakubeeee.testutils.marker.IntegrationTestCategory;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Abstract superclass that provides basic functionality for integration tests.
+ */
+@Category(IntegrationTestCategory.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = Application.class)
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class
+})
+@AutoConfigureMockMvc
+@ActiveProfiles("integrationtest")
+@PropertySource(value = "classpath:generated/testdatabase-generated.properties", ignoreResourceNotFound = true)
+public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    private static final boolean AUTOCONFIGURE_TEST_DATABASE = true;
+
+    static {
+        if (AUTOCONFIGURE_TEST_DATABASE)
+            CustomPostgreSQL12Container.getInstance().start();
+    }
+
+}
--- testcore/src/main/java/module-info.java
+++ testcore/src/main/java/module-info.java
@@ -1,12 +1,21 @@
 module testcore {
     requires common;
     requires core;
+    requires static testutils;
 
     requires database.commons;
     requires junit;
     requires org.codehaus.groovy;
     requires spring.core;
     requires testcontainers.merged;
+    requires static lombok;
+    requires static spring.beans;
+    requires static spring.context;
+    requires static spring.test;
+    requires static spring.boot.test.autoconfigure;
+    requires static spring.boot.test;
+    requires static spring.test.dbunit;
 
     exports com.jakubeeee.testcore.container;
+    exports com.jakubeeee.testcore.test;
 }
