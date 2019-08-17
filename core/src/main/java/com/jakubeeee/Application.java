0c475ab6aa5596d5caba125e5d6a8a6f67cde093
Jakub Bereźnicki <jakub.bereznicki@gmail.com>
2019-08-17 19:52:52 +0200

    Removed unused import

--- core/src/main/java/com/jakubeeee/Application.java
+++ core/src/main/java/com/jakubeeee/Application.java
@@ -3,7 +3,6 @@ package com.jakubeeee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Class that serves as the entry point of the application.
