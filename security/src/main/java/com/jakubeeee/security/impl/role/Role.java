48749f47b5555a01ba27ad64a590ed03667f4e7c
jakubeeee <jakub.bereznicki@gmail.com>
2019-10-13 19:16:00 +0200

    Removed Role's cyclic references to users

--- security/src/main/java/com/jakubeeee/security/impl/role/Role.java
+++ security/src/main/java/com/jakubeeee/security/impl/role/Role.java
@@ -1,15 +1,12 @@
 package com.jakubeeee.security.impl.role;
 
 import com.jakubeeee.common.persistence.IdentifiableEntity;
-import com.jakubeeee.security.impl.user.User;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Entity representing role that can be assigned to users. They allow access to system
@@ -26,12 +23,6 @@ public class Role extends IdentifiableEntity {
     @Column(name = "TYPE", unique = true, nullable = false, updatable = false)
     @Enumerated(EnumType.STRING)
     private Type type;
-
-    // exclude from equals, hashCode and toString methods to prevent cyclic references with users
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users = new HashSet<>();
 
     private Role(Type type) {
         this.type = type;
